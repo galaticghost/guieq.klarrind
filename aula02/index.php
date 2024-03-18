58ea3da12619b26196bfd6645662e4f775eaf6c0
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-03-18 11:40:36 -0300

    aula 02

---
+++ aula02/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $carros = array("doblo" , "opala", "renegade");
+    $size = count($carros);
+    
+    for ($i = 0; $i < $size; $i++) {
+        echo $carros[$i] . "<br>";
+    }
+    $carros[0] = "uno";
+    echo $carros[0];
+?>
+
+<br>
