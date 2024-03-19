b282782df352cb39778d5fb8fe656d33c0b5aae7
Arthur <arthurvonrohrmonteiro@gmail.com>
2024-03-19 13:32:49 -0300

    2

---
+++ index2.php
@@ -0,0 +1,17 @@
+<?php
+    $frutas = array("Tomate", "Maçã", "Banana", "Laranja", "Bergamota", "Arroz", "Pêra");
+    $size = count($frutas);
+    $i = 0;
+
+    do {
+        if ($frutas[$i] == "Arroz"){
+            echo "Arroz não é uma fruta?<br>";
+            $i++;
+        }
+        else {
+            echo $frutas[$i] . "<br>";
+            $i+= 1;
+        }
+    }
+    while ($i < $size);
+?>
