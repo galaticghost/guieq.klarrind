879d79eeffe812eccffe78f3584b98fe7efaebdc
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-03-19 11:00:54 -0300

    Potência chefia

--- Array/hoje.php
+++ Array/hoje.php
@@ -1,17 +1,19 @@
 <?php
+    //Váriaveis e arrays e afins e kfimeom,2!
     date_default_timezone_set("America/Sao_Paulo");
-    $semana = array("domingo","segunda","terça","quarta","quinta","sexta",
-    "sábado");
+    $semana = ["domingo","segunda","terça","quarta","quinta","sexta",
+    "sábado"];
     $diaDaSemana = date("w");
-    $hoje = date("d");
-    $mes = date("n");
-    $ano = date("Y");
     $day = $semana[$diaDaSemana]; 
-    $meses = array("Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro");
+    $meses = ["janeiro","fevereiro","março","abril","maio","junho",
+    "julho","agosto","setembro","outubro","novembro","dezembro"];
+    $mes = date("n");
     $mes = $meses[$mes-1];
 
+    // Bomdias e Dias alfabéticos
     echo "Bom dia! Hoje é " . $semana[$diaDaSemana];
     
+    //Feiras ou espaços
     if ($day == "Domingo" or $day == "Sábado") {
         echo "";
     } 
@@ -19,7 +21,9 @@
         echo "-feira";
     }
     
-    echo ", dia " . $hoje . " do mês de " . $mes . " do ano de " . $ano;
+    //afins
+    echo ", dia " . date("d") . " do mês de " . $mes . " do ano de " . date("Y");
 
 
 ?>
+<br>
---
+++ teste_de_potencia.php
@@ -0,0 +1,8 @@
+<?php
+    $n1 = 3;
+    $n2 = pow($n1, 2) + 2;
+    echo "$n1 <br>";
+    echo pow($n1, 2) . "<br>";
+    echo $n2;
+
+?>
