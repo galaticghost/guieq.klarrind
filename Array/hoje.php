9ceaab3f335becfc9515822f616086b42d49cf67
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-03-19 11:03:45 -0300

    Agora com menos váriaveis!

--- Array/hoje.php
+++ Array/hoje.php
@@ -1,20 +1,18 @@
 <?php
-    //Váriaveis e arrays e afins e kfimeom,2!
+    //Váriaveis e arrays e afins e kfimeom,2! e 
     date_default_timezone_set("America/Sao_Paulo");
     $semana = ["domingo","segunda","terça","quarta","quinta","sexta",
     "sábado"];
     $diaDaSemana = date("w");
-    $day = $semana[$diaDaSemana]; 
     $meses = ["janeiro","fevereiro","março","abril","maio","junho",
     "julho","agosto","setembro","outubro","novembro","dezembro"];
     $mes = date("n");
-    $mes = $meses[$mes-1];
 
     // Bomdias e Dias alfabéticos
     echo "Bom dia! Hoje é " . $semana[$diaDaSemana];
     
     //Feiras ou espaços
-    if ($day == "Domingo" or $day == "Sábado") {
+    if ($semana[$diaDaSemana] == "Domingo" or $semana[$diaDaSemana] == "Sábado") {
         echo "";
     } 
     else {
@@ -22,7 +20,7 @@
     }
     
     //afins
-    echo ", dia " . date("d") . " do mês de " . $mes . " do ano de " . date("Y");
+    echo ", dia " . date("d") . " do mês de " . $meses[$mes-1] . " do ano de " . date("Y");
 
 
 ?>
