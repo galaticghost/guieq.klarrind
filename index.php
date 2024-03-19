34af81dc4bde4be8c33434b8788a37f1f9622302
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-03-19 09:10:36 -0300

    index atualizado

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php
-    $frutas = array("Tomate","Maçã","Banana","Pêra","Torta");
-    
-    $size = count($frutas);
-    
-    for ($i = 0; $i < $size; $i++) {
-        echo $frutas[$i] . "<br>";
+    $numerospoidia = 0;
+    $i2 = 0;
+    echo "hypériofront";
+    while ($i2 < 6){
+        echo "/2/293/482,2";
+            $i2 ++;
     }
-    echo $size;
+    $i2 = 0;
+    for($i = 0; $i < 20; $i++){
+        $numerospoidia = 1;
+        if ($numerospoidia == 1) {
+            echo "Numérospódia $i<br>";
+            $numerospoidia = $i;
+        }
+        if ($numerospoidia % 2 == 0) {
+            echo "guieq";
+        }
+        else {
+            echo "klarrind";
+        }
+        while ($i2 < 7) {
+            echo "/2/293/482,2";
+            $i2 ++;
+        }
+        $i2 = 0;
+    }
+    echo "Numérospódia 20";
 ?>
 <br>
