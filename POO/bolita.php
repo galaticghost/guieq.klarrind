9d8044eae0c5c485763df4e69a228d03fa6e0fe1
Arthur <arthurvonrohrmonteiro@gmail.com>
2024-04-01 14:55:02 -0300

    Teste pêra

--- POO/bolita.php
+++ POO/bolita.php
@@ -6,4 +6,6 @@ include_once("clsProduto.php");
     $c1 = new categoria(01, "Frutas");
     
     $p1 = new produto(01, "Maçã", 3.3, 4, $c1);
-    echo $p1->name;
+    $p3 = new produto(03, "Pêra", 3.2, 4, $c1);
+    echo $p1->name . " - Preço: R$" . $p1->preco;
+    echo "<br>" . $p3->name . " - Preço: R$" . $p3->preco;
