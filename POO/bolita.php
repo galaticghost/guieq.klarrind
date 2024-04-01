abb99b0d2dec0633d3d422e10ce7bf6c8ea1908f
Arthur <arthurvonrohrmonteiro@gmail.com>
2024-04-01 14:47:51 -0300

    banana

--- POO/bolita.php
+++ POO/bolita.php
@@ -6,4 +6,7 @@ include_once("clsProduto.php");
     $c1 = new categoria(01, "Frutas");
     
     $p1 = new produto(01, "Maçã", 3.3, 4, $c1);
-    echo $p1->name;
+    $p2 = new produto(02, "Banana", 4.1, 5, $c1);
+
+    echo $p1->name . " - Preço R$:" . $p1->preco;
+    echo $p2->name . " - Preço R$:" . $p2->preco;
