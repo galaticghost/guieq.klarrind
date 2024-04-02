ddd4345c0bd6130224ba21a75c7b92884de95f3e
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-04-02 10:14:01 -0300

    praga2

--- POOTWO/index.php
+++ POOTWO/index.php
@@ -1,11 +1,8 @@
 <?php
     require_once("carro.php");
 
-    $car1 = new carro("2018", "Fiat", "Doblo", "Vermelho"); 
-    $car2 = new carro("2014", "Honda", "Fit", "Prata");
+    $carros = array(
+        new carro("2019", "Fiat", "Cronos", "Vermelho", "Automático"),
+        new carro("")
 
-    $car1->status();
-    $car2->status();
-
-    $car1->vender(70000);
-    $car2->vender(50000);
+    );
