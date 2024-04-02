f1e5cf2573d3bbce6e152a7efae34b2abf4fbde2
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-04-02 10:51:15 -0300

    fun.php

--- POOTWO/fun!!!.php
+++ POOTWO/fun!!!.php
@@ -1,2 +1,10 @@
 <?php
-    
+    $n1 = rand(1, 100000);
+    for($i = 0;$i <= 100000;$i++){
+        if ($i == $n1){
+            echo "<br>Fun!!!!!";
+        }
+        else {
+            echo "<br>praga";
+        }
+    }
