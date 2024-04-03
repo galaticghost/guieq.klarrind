263b77fa930c98430fe703328f2ff2d60557e8a1
Arthur <arthurvonrohrmonteiro@gmail.com>
2024-04-03 09:55:42 -0300

    trava pc

--- POOTWO/fun!!!.php
+++ POOTWO/fun!!!.php
@@ -1,10 +1,33 @@
 <?php
-    $n1 = rand(1, 100000);
-    for($i = 0;$i <= 100000;$i++){
-        if ($i == $n1){
-            echo "<br>Fun!!!!!";
+//Varíolas/números randomicos    
+    $n1 = rand(1,400);
+    $n2 = rand(5,400);
+    $n3 = rand(68,69);
+//Caso n1 e n2 forem iguais  
+    while ($n1 == $n2) {
+        $n1 = rand(1,400);
+        $n2 = rand(5,400);
+    }
+// trava pc
+    for($i = 0;$i <= ($n1+$n2);$i++){
+        echo "<br>";
+        if ($n3 == 68) {
+            echo "Bolita==Guieq/Klarrind" . $n3 . "*//*/<>S<>#%53,.<>#¨#<L@>!%#<>k®y┼`µgiso3k/Bola|369|bolA\ ";
+            if ($n1 > $n2) {
+                for($i2 = 1;$i2 < 10;$i*2){
+                    echo "<br>";
+                    echo "%!Klarrind!%";
+                    echo rand(273,789406);
+                } 
+            }
         }
         else {
-            echo "<br>praga";
+            echo "<br>Charlaros == CARLINISMO" . $n3 . "!&*%K!&KODoihjOGKSDIJIEWPFMJK<><><>D<><G><S><DG><S>D<>GD<>GDS<><E><>E<>#@<><><>D<S><><X>Z<{FX<E<<GS>G<>LEHNjdklmklgfmrootjewkjoewjtewop/" . $n1 . "0935023";
+            if ($n1 > $n2) {
+                for($i2 = 1;$i2 < 50;$i*2){
+                    echo "%!Klarrind!%";
+                    echo rand(273,789406);
+                } 
+            }
         }
     }
