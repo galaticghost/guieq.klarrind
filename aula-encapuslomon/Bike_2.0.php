25111e55bc36f92b28641bf450d6a6124c8e6ce0
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-04-09 10:56:42 -0300

    TESTE SUPER FUCK(do ingles super foda)

--- aula-encapuslomon/Bike_2.0.php
+++ aula-encapuslomon/Bike_2.0.php
@@ -13,9 +13,10 @@ class Bicicleta{
 		$this->marca = $marca;
 		$this->modelo = $modelo;
 		$this->cor = $cor;
-		$this->estado = "parado";
+		$this->estado = "Parado";
 	}
 
+	//VRUMVRUM(mais ecológico(maisoumentos))
 	public function andar($velocidade){
 		if ($velocidade >= 0 ){
 			$this->velocidade += $velocidade;
@@ -23,30 +24,39 @@ class Bicicleta{
 		else{
 			$this->velocidade += 0;
 		}
-		echo "Agora a bicicleta anda a $velocidade (Kilometros por hora?Metros por segundo?)<br>";
+		echo "Agora a bicicleta anda a $this->velocidade (Kilometros por hora?Metros por segundo?)<br>";
+		$this->estado = "Andando";
 	}
 
+	//vrumvrum minusculo
 	public function frear($velocidade){
-		if($velocidade >= 0){
-			$this->velocidade =- $velocidade;
+		if($velocidade <= $this->velocidade){
+			$this->velocidade -= $velocidade;
 		}
 		else{
-			$this->velocidade =- $velocidade;
+			$this->velocidade = 0;
 		}
+		echo "Agora a bicicleta está a $this->velocidade (por favor coloca o tipo de velocidade)";
 	}
 	//A magia (não exatamente) acontece aqui
 	public function status($bicicleta){
 		echo "Id: " . $bicicleta->id;
 		echo "<br>Marca: " . $bicicleta->marca;
 		echo "<br>Modelo: " . $bicicleta->modelo;
-		echo "<br>Cor: " . $bicicleta->cor . "<br>";
+		echo "<br>Cor: " . $bicicleta->cor;
+		echo "<br>Estado: " . $bicicleta->estado . "<br>";
+		}
+	//Status unicamente do estado de movimento
+	public function estadoStatus($bicicleta){
+		echo "Estado: " . $bicicleta->estado . "<br>";
 	}
-
 }
 //Bicicletas
 $bicicleta = new Bicicleta ("01","Caloi","SuperFoda","verde");
 
-//Filmes não audio e nem moventos
+//Filmes sem audio e sem moventos
 echo "<h2>Bicicletas</h2>";
 $bicicleta->status($bicicleta);
 $bicicleta->andar(50);
+$bicicleta->estadoStatus($bicicleta);
+$bicicleta->frear(60);
