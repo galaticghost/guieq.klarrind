4cfb48a66618c10b1500322c5963ff8e5f8fb6ec
Arthur <arthurvonrohrmonteiro@gmail.com>
2024-04-10 08:23:45 -0300

    bike2.1

--- aula-encapuslomon/Bike_2.0.php
+++ aula-encapuslomon/Bike_2.0.php
@@ -24,7 +24,7 @@ class Bicicleta{
 		else{
 			$this->velocidade += 0;
 		}
-		echo "Agora a bicicleta anda a $this->velocidade (Kilometros por hora?Metros por segundo?)<br>";
+		echo "<br>Agora a bicicleta anda a $this->velocidade (Kilometros por hora?Metros por segundo?)";
 		$this->estado = "Andando";
 	}
 
@@ -32,27 +32,39 @@ class Bicicleta{
 	public function frear($velocidade){
 		if($velocidade <= $this->velocidade){
 			$this->velocidade -= $velocidade;
+			$this->estado = "Freando";
+			echo "<br>Agora a bicicleta está a $this->velocidade (por favor coloca o tipo de velocidade)";
 		}
 		else{
 			$this->velocidade = 0;
+			$this->estado = "Parado";
+			echo "<br>Agora a bicicleta está parada";
+
 		}
-		echo "Agora a bicicleta está a $this->velocidade (por favor coloca o tipo de velocidade)";
 	}
+	//Parar
+	public function parar(){
+		$this->velocidade = 0;
+		$this->estado = "Parado";
+		echo "<br>Agora a bicicleta está parada";
+	}
+
 	//A magia (não exatamente) acontece aqui
 	public function status($bicicleta){
 		echo "Id: " . $bicicleta->id;
 		echo "<br>Marca: " . $bicicleta->marca;
 		echo "<br>Modelo: " . $bicicleta->modelo;
 		echo "<br>Cor: " . $bicicleta->cor;
-		echo "<br>Estado: " . $bicicleta->estado . "<br>";
+		echo "<br>Estado: " . $bicicleta->estado;
 		}
+
 	//Status unicamente do estado de movimento
 	public function estadoStatus($bicicleta){
-		echo "Estado: " . $bicicleta->estado . "<br>";
+		echo "<br>Estado: " . $bicicleta->estado;
 	}
 }
 //Bicicletas
-$bicicleta = new Bicicleta ("01","Caloi","SuperFoda","verde");
+$bicicleta = new Bicicleta ("01","Caloi","SuperFoda","Verde");
 
 //Filmes sem audio e sem moventos
 echo "<h2>Bicicletas</h2>";
@@ -60,3 +72,4 @@ $bicicleta->status($bicicleta);
 $bicicleta->andar(50);
 $bicicleta->estadoStatus($bicicleta);
 $bicicleta->frear(60);
+$bicicleta->estadoStatus($bicicleta);
