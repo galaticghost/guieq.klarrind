57552657731c6b803f3cc9ae5ad58a848bfa90e5
galaticghost <arthurvonrohrmonteiro@gmail.com>
2024-04-22 11:38:24 -0300

    loja.v0.3

--- loja/controller/salvarCidade.php
+++ loja/controller/salvarCidade.php
@@ -6,5 +6,12 @@ if (strlen($nome) == 0){
     header("Location: ../cidades.php?nomeVazio");
 }
 else{
+    include_once("../dao/clsConexao.php");
+    include_once("../dao/clsCidadeDAO.php");
+    include_once("../model/clsCidade.php");
+
+    $novaCid = new Cidade();
+    $novaCid->nome = $nome;
+    CidadeDAO::inserir($novaCid);
     header("Location: ../cidades.php?nome=$nome");
 }
